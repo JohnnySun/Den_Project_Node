8f7ca7a93901b7af8b478208a268d10366d3e8ff
JohnnySun <bmy001@gmail.com>
2014-08-27 17:56:04 +0800

    增加COPYING,使用GPL协议发布。为所有文件增加说明

--- sendpackage.cpp
+++ sendpackage.cpp
@@ -1,3 +1,12 @@
+/*	A part of node
+	Den Project
+	Copyright 2007-2014 BMY-Soft
+
+	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+*/
+
 /*
    规定数据包格式
    (NodeName)#END(Data Types)#END(Data Name)#END(Data)#END#EOF
